e112b2575c8a577cc4f1b91a362e4c4764849dd8
Ahmadalikhan001 <ahmadalik282@email.com>
2024-06-24 11:22:37 +0500

    Add New File

---
+++ javascript.js
@@ -0,0 +1,24 @@
+console.log("Hellow World")
+console.log("This is My first Program in Javascript")
+let a=4
+let b=6
+let c=7
+console.log(a+b+c)
+d= a+b+c
+console.log(d)
+let a1= 23
+let b1= "Ahmad" 
+console.log(a1+b1)
+console.log (typeof(a1+b1))
+const a2= {Name:"Ahmad Ali Khan",Section:"1",Principal:false}
+a2["friend"]="Ayin"
+console.log(a2)
+const b2={Name:"Ahmad Ali Khan",F_Name:"Nek Dar Ali Khan",Age:"26"}
+b2["village"]="Lalozai"
+console.log(b2)
+console.log(typeof(b2))
+let a3 = prompt("Enter The First Value=    ");
+let b3 = prompt("Enter The 2nd Value=    ");
+let c3 = Number(a3) + Number(b3);
+console.log(c3);
+
